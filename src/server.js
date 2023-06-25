a88db81cafcc33e78e86ead8f2d4b6f7b8583e38
thuhuong1234 <hdnguyenthuhuong1234@gmail.com>
2023-06-26 00:41:40 +0700

    install express- set up env

---
+++ src/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+dotenv.config();
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+ 
+ 
+ 
+    console.log("Example app listening on port", `${port}`);
+});
