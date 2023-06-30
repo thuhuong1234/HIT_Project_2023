ba103c36b23963eeff87a56cf8769efeed92c72a
thuhuong1234 <hdnguyenthuhuong1234@gmail.com>
2023-07-01 00:28:26 +0700

    Add field  email into member.model

--- src/models/members.model.js
+++ src/models/members.model.js
@@ -1,38 +1,66 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const validator = require("validator");
+const memberSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Invalid email");
+        }
+      },
+    },
+    dateOfBirth: {
+      type: Date,
+    },
+    gender: {
+      type: String,
+    },
+    image: {
+      type: String,
+    },
+    address: {
+      type: String,
+    },
+    role: {
+      type: String,
+      enums: ["leader", "member", "support"],
+      default: "member",
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 8,
+      validate(value) {
+        if (!value.match(/\d/) || !value.match(/[a-zA-Z]/)) {
+          throw new Error(
+            "Password must contain at least one letter and one number"
+          );
+        }
+      },
+    },
+    studentCode: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const memberSchema = new Schema({
+memberSchema.statics.isEmailTaken = async function (email) {
+  const member = await this.findOne({ email });
+  return !!member;
+};
 
-  name: {
-    type: String,
-    required: true,
-  },
-  dateOfBirth: {
-    type: Date,
-  },
-  gender: {
-    type: String,
-  },
-  image: {
-    type: String,
-  },
-  address: {
-    type: String,
-  },
-  role: {
-    type: String,
-    enums: ["leader", "member", "support"],
-    default: "member",
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  studentCode: {
-    type: String,
-    required: true,
-  },
-});
 const Member = mongoose.model("Member", memberSchema);
 
 module.exports = Member;
--- src/services/member.service.js
+++ src/services/member.service.js
@@ -1,8 +1,8 @@
 const Member = require("../models/members.model");
-const apiError = require("../utils/apiError");
+const ApiError = require("../utils/apiError");
 const httpStatus = require("http-status");
 const APIFeatures = require("../utils/apiFeatures");
-
+const validator = require('validator')
 const getMembers = async (query) => {
   const feature = new APIFeatures(Member.find(), query).paginate();
   return feature.query;
@@ -10,39 +10,38 @@ const getMembers = async (query) => {
 const getMember = async (memberId) => {
   const member = await Member.findById(memberId);
   if (!member) {
-    throw new apiError(httpStatus.NOT_FOUND, "Member not found!");
+    throw new ApiError(httpStatus.NOT_FOUND, "Member not found!");
   }
   return member;
 };
 const createMember = async (newMember) => {
-  if (!newMember.name || !newMember.password || !newMember.studentCode) {
-    throw new apiError(
-      httpStatus.BAD_REQUEST,
-      "Member's information is not enough!"
-    );
+  if (
+    !newMember.name || !newMember.password ||!newMember.studentCode ||!newMember.email) {
+    throw new ApiError( httpStatus.BAD_REQUEST,"Member's information is not enough!");
   }
 
-  const checkMember = await Member.findOne({
-    studentCode: newMember.studentCode,
-  });
-  if (checkMember) {
-    throw new apiError(httpStatus.BAD_REQUEST, "Member already exists!");
+  if (await Member.findOne({ studentCode:newMember.studentCode })) {
+    throw new ApiError(httpStatus.BAD_REQUEST, "StudentCode already exists!");
   }
-
+  if (await Member.isEmailTaken(newMember.email)) {
+    throw new ApiError(httpStatus.BAD_REQUEST, "'Email already taken')!");
+  }
   const member = await Member.create(newMember);
   return member;
 };
+
 const updateMember = async (memberId, updateMember) => {
   const member = await Member.findByIdAndUpdate(memberId, updateMember);
   if (!member) {
-    throw new apiError(httpStatus.NOTFOUND, "Member not found!");
+    throw new ApiError(httpStatus.NOTFOUND, "Member not found!");
   }
   return member;
 };
+
 const deleteMember = async (memberId) => {
   const member = await Member.findByIdAndDelete(memberId);
   if (!member) {
-    throw new apiError(httpStatus.NOTFOUND, "Member not found!");
+    throw new ApiError(httpStatus.NOTFOUND, "Member not found!");
   }
   return member;
 };
